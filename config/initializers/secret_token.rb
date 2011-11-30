220c485c656f9b692d908d8f9672bb8f953f9475
Do Young Min <cto@smato.org>
2011-11-30 17:27:49 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '92c85adc872af5d7ae696e4081b66291d59c3935cebdcb9b7f6d5310381de5756bdb5c111c5197d990f4b15b202580383da54cf374d1431197a63901b454e4e4'
